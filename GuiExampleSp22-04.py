1b12f73bc4a921d8a57506a8fdcd0680a5b4ca34
Ethan Debnath <ethan.debnath@yahoo.com>
2023-12-04 22:59:53 -0600

    new

--- GuiExampleSp22-04.py
+++ GuiExampleSp22-04.py
@@ -12,6 +12,124 @@ def db_connect():
     return sqlite3.connect('EthanDb.db')
 
 
+def list_borrowers_with_late_fee(search_term=''):
+    conn = db_connect()
+    cur = conn.cursor()
+
+    sql_query = """
+    SELECT 
+        br.Card_No AS BorrowerID, 
+        br.Name AS BorrowerName, 
+        COALESCE('$' || CAST(ROUND(SUM(CASE 
+            WHEN bl.Returned_date > bl.Due_Date THEN 
+                (JULIANDAY(bl.Returned_date) - JULIANDAY(bl.Due_Date)) * lb.LateFee 
+            ELSE 0 
+        END), 2) AS TEXT), '$0.00') AS LateFeeBalance
+    FROM 
+        BORROWER br
+    LEFT JOIN 
+        BOOK_LOANS bl ON br.Card_No = bl.Card_No
+    LEFT JOIN 
+        LIBRARY_BRANCH lb ON bl.Branch_Id = lb.Branch_Id
+    GROUP BY 
+        br.Card_No
+    HAVING 
+        (? IS NULL OR ? = '') OR 
+        (br.Card_No = ?) OR 
+        (br.Name LIKE '%' || ? || '%')
+    ORDER BY 
+        LateFeeBalance DESC;
+    """
+
+    try:
+        cur.execute(sql_query, (search_term, search_term, search_term, search_term))
+        records = cur.fetchall()
+        display_records_in_popup(records)
+    except sqlite3.Error as e:
+        messagebox.showerror("Error", str(e))
+    finally:
+        conn.close()
+
+def display_records_in_popup(records):
+    popup = tk.Toplevel(root)
+    popup.title("Query Results")
+    popup.geometry("600x400")
+
+    text_widget = tk.Text(popup)
+    text_widget.pack(expand=True, fill='both')
+
+    scrollbar = tk.Scrollbar(popup, command=text_widget.yview)
+    scrollbar.pack(side='right', fill='y')
+    text_widget.config(yscrollcommand=scrollbar.set)
+
+    for record in records:
+        text_widget.insert(tk.END, f"Borrower ID: {record[0]}, Name: {record[1]}, Late Fee Balance: {record[2]}\n")
+
+# Search field
+search_label = tk.Label(root, text="Enter Borrower ID or Name:")
+search_label.pack()
+
+search_entry = tk.Entry(root)
+search_entry.pack()
+
+# Search button
+search_button = tk.Button(root, text="Search", command=lambda: list_borrowers_with_late_fee(search_entry.get()))
+search_button.pack()
+
+#-----------------6b
+
+
+def list_book_information(borrower_id='', book_id='', book_title=''):
+    conn = db_connect()
+    cur = conn.cursor()
+
+    sql_query = """
+    SELECT 
+        b.Book_Id, 
+        b.Title, 
+        COALESCE('$' || CAST(ROUND((JULIANDAY(bl.Returned_date) - JULIANDAY(bl.Due_Date)) * lb.LateFee, 2) AS TEXT), 'Non-Applicable') AS LateFeeAmount
+    FROM 
+        BOOK b
+    JOIN 
+        BOOK_LOANS bl ON b.Book_Id = bl.Book_Id
+    JOIN 
+        BORROWER br ON bl.Card_No = br.Card_No
+    JOIN 
+        LIBRARY_BRANCH lb ON bl.Branch_Id = lb.Branch_Id
+    WHERE 
+        (br.Card_No = ? OR ? = '') AND
+        (b.Book_Id = ? OR ? = '') AND
+        (b.Title LIKE '%' || ? || '%' OR ? = '')
+    ORDER BY 
+        LateFeeAmount DESC;
+    """
+
+    try:
+        cur.execute(sql_query, (borrower_id, borrower_id, book_id, book_id, book_title, book_title))
+        records = cur.fetchall()
+        display_records_in_popup(records)
+    except sqlite3.Error as e:
+        messagebox.showerror("Error", str(e))
+    finally:
+        conn.close()
+
+def display_records_in_popup(records):
+    popup = tk.Toplevel(root)
+    popup.title("Book Information")
+    popup.geometry("600x400")
+
+    text_widget = tk.Text(popup)
+    text_widget.pack(expand=True, fill='both')
+
+    scrollbar = tk.Scrollbar(popup, command=text_widget.yview)
+    scrollbar.pack(side='right', fill='y')
+    text_widget.config(yscrollcommand=scrollbar.set)
+
+    for record in records:
+        text_widget.insert(tk.END, f"Book ID: {record[0]}, Title: {record[1]}, Late Fee: {record[2]}\n")
+
+#-----------------6b
+
 def add_new_book():
     conn = db_connect()
     cur = conn.cursor()
@@ -45,10 +163,24 @@ def add_new_book():
         messagebox.showerror("Error", str(e))
     finally:
         conn.close()
-        
 
+borrower_id_label = tk.Label(root, text="Borrower ID:")
+borrower_id_label.pack()
+borrower_id_entry = tk.Entry(root)
+borrower_id_entry.pack()
 
+book_id_label = tk.Label(root, text="Book ID:")
+book_id_label.pack()
+book_id_entry = tk.Entry(root)
+book_id_entry.pack()
 
+book_title_label = tk.Label(root, text="Book Title:")
+book_title_label.pack()
+book_title_entry = tk.Entry(root)
+book_title_entry.pack()
+
+search_button = tk.Button(root, text="Search", command=lambda: list_book_information(borrower_id_entry.get(), book_id_entry.get(), book_title_entry.get()))
+search_button.pack()
 
 # Function to list the number of copies loaned out per branch for a given book title
 def list_copies_loaned_out():
@@ -175,6 +307,15 @@ canvas.create_window((0,0), window=frame, anchor='nw')
 # GUI Components inside the frame
 add_book_heading = tk.Label(frame, text="Add a New Book", font=("Helvetica", 16, "bold"))
 add_book_heading.pack(pady=10)
+
+
+# Set up Treeview for displaying results
+columns = ('BorrowerID', 'BorrowerName', 'LateFeeBalance')
+tree = ttk.Treeview(root, columns=columns, show='headings')
+for col in columns:
+    tree.heading(col, text=col)
+    tree.column(col, width=100)
+tree.pack(fill='both', expand=True)
 
 # GUI Components for adding a new book
 title_label = tk.Label(frame, text="Book Title:")
